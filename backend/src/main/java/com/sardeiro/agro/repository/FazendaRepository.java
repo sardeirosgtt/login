1b5dcd5d72fa42a76222592d55dfdff1c824f5d5
sardeiro <ddandre1010@gmail.com>
2025-04-05 20:04:13 -0300

    feat: criando repository da fazenda

---
+++ backend/src/main/java/com/sardeiro/agro/repository/FazendaRepository.java
@@ -0,0 +1,9 @@
+package com.sardeiro.agro.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sardeiro.agro.domain.Fazenda;
+
+public interface FazendaRepository extends JpaRepository<Fazenda, Long>{
+
+}
